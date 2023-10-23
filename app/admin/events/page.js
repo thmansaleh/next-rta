c36dc81914d2e50329160589a63622c6b4b87238
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2023-10-23 16:21:59 +0400

    Update page.js

--- app/admin/events/page.js
+++ app/admin/events/page.js
@@ -2,7 +2,7 @@ export default function Events() {
 
 const data = fetch('https://dull-plum-antelope-tutu.cyclic.cloud/api/events')
   .then(response => response.json()) 
-  .then(data => return data)
+  .then(data =>{ return data})
 
  return <h1>othman saleh</h1>
 }
