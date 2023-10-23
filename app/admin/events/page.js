3ab9a9dfa209649ea75af71bdd003b350067827f
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2023-10-23 16:06:12 +0400

    Update page.js

--- app/admin/events/page.js
+++ app/admin/events/page.js
@@ -1,15 +1,6 @@
 import Modal from "./event-detailes/Modal";
 import EventsTable from "./EventsTable";
 
-export default async function Events() {
-
-   const url= 'https://dull-plum-antelope-tutu.cyclic.cloud'
-const res = await fetch('https://dull-plum-antelope-tutu.cyclic.cloud/api/events' ,{
-  next: { revalidate: 1 }, // Revalidate every 60 seconds
-}
-)
-  const data = await res.json()
-
 
   return (<h1>111111<h1>
   //  <EventsTable data={data}/>
