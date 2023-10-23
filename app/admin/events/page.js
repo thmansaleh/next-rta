913e5f12a44ec96f52fac1e3f3b0b2ea9cd32205
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2023-10-23 16:07:22 +0400

    Update page.js

--- app/admin/events/page.js
+++ app/admin/events/page.js
@@ -1,9 +1,5 @@
-import Modal from "./event-detailes/Modal";
-import EventsTable from "./EventsTable";
 
 
-  return (<h1>111111<h1>
-  //  <EventsTable data={data}/>
-   )
+  return (<h1>111111<h1>  )
 }
 
