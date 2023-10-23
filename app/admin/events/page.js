526ad859f0b52d9a2b68d1c88081bff11abaa392
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2023-10-23 16:19:04 +0400

    Update page.js

--- app/admin/events/page.js
+++ app/admin/events/page.js
@@ -1,3 +1,8 @@
 export default function Events() {
+
+const data = fetch('https://example.com/api/data')
+  .then(response => response.json()) 
+  .then(data => return data)
+
  return <h1>othman saleh</h1>
 }
