8330a860e0aa9342e766cda969418179c0caa6ce
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2023-10-23 16:19:30 +0400

    Update page.js

--- app/admin/events/page.js
+++ app/admin/events/page.js
@@ -1,6 +1,6 @@
 export default function Events() {
 
-const data = fetch('https://example.com/api/data')
+const data = fetch('https://dull-plum-antelope-tutu.cyclic.cloud/api/events')
   .then(response => response.json()) 
   .then(data => return data)
 
