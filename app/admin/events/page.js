ab0eeebde9230b5c19e13bb6fba29827f667b8c7
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2023-10-23 15:52:30 +0400

    Update page.js

--- app/admin/events/page.js
+++ app/admin/events/page.js
@@ -3,8 +3,7 @@ import EventsTable from "./EventsTable";
 
 export default async function Events() {
 
-  // const url= true?process.env.URL:'http://localhost:3000'
-  const url= 'https://next-eight-xi.vercel.app'
+   const url= 'https://dull-plum-antelope-tutu.cyclic.cloud'
 const res = await fetch(`${url}/api/events` ,{
   next: { revalidate: 0 }, // Revalidate every 60 seconds
 }
