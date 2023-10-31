b234b49ab1e3e45e7bf6e7834f5ffa551a18b9bb
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2023-11-01 02:44:29 +0400

    Update TableItem.js

--- app/admin/events/TableItem.js
+++ app/admin/events/TableItem.js
@@ -24,7 +24,7 @@ function TableItem({data}) {
     </td>
     <td className=" py-2">
       <Modal data={data}/>
-      <DeleteModal id={data}/>
+      <DeleteModal id={data.id}/>
            </td>
   </tr>
 
