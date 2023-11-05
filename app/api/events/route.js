d8a72ae54e8e12c71c6ae06a4a83378c31feb497
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2023-11-06 01:22:57 +0400

    Update route.js

--- app/api/events/route.js
+++ app/api/events/route.js
@@ -3,17 +3,28 @@ import deleteEvent from "@/app/libs/deleteEvent"
 export async  function GET() {
 const token = localStorage.getItem('token')
 
-        const res = await fetch(`https://dull-plum-antelope-tutu.cyclic.cloud/api/events`,
-    {
-method: 'GET',
-headers: {
+        fetch('https://dull-plum-antelope-tutu.cyclic.cloud/events', {
+        method: 'GET',
+        headers: {
             'Authorization': token,
             'Content-Type': 'application/json'
         }
-    }
-    )
-const data =await res.json()
-    return Response.json( data)
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(data => {
+        
+return Response.json( data)
+    })
+    .catch(error => {
+        console.error('There has been a problem with your fetch operation:', error);
+    });
+
+    
   }
 
 
