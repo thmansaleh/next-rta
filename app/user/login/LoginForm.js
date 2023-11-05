208b68eff35eed6b812f21965eef1575ea1d8849
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2023-11-05 16:35:57 +0400

    Update LoginForm.js

--- app/user/login/LoginForm.js
+++ app/user/login/LoginForm.js
@@ -1,6 +1,6 @@
 'use client'
 import { useState } from 'react';
-import Link from 'next/link
+import Link from 'next/link'
 const LoginForm = () => {
   
 const [username, setUsername] = useState('');
