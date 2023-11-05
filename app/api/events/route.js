26dd552c7f753d7edc99d2eab62f1a1069f46796
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2023-11-05 19:54:18 +0400

    Update route.js

--- app/api/events/route.js
+++ app/api/events/route.js
@@ -1,14 +1,8 @@
 import deleteEvent from "@/app/libs/deleteEvent"
 
-export async function GET() {
+export  function GET() {
 
-    const res = await fetch('https://dull-plum-antelope-tutu.cyclic.cloud/events',
-{ 
-next: { revalidate: 1}
-}
-    )
-    const data = await res.json()
-   
+    
     return Response.json( "othman")
   }
 
