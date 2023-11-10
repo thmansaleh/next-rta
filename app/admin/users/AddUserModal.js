bb82f77b2bc1b2dabc6efa0b4e90da1d7d866772
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2023-11-10 17:46:30 +0400

    Update AddUserModal.js

--- app/admin/users/AddUserModal.js
+++ app/admin/users/AddUserModal.js
@@ -1,5 +1,6 @@
 'use client'
 import React from "react";
+import ModalForm from "./ModalForm"
 
 export default function AddUserModal() {
   const [showModal, setShowModal] = React.useState(false);
@@ -22,7 +23,7 @@ export default function AddUserModal() {
               {/*content*/}
               <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
             
-             
+         <ModalForm/>    
                 <div className="flex items-center justify-end p-6 border-t border-solid border-blueGray-200 rounded-b">
                 <button
                     className="bg-red-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
